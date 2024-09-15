1d797eadd42ec3509978766138c4a5ab6f1bd86e
Bradley Floyd III <bfloydiii@protonmail.com>
2024-09-15 00:21:51 -0400

    first commit

---
+++ intro.js
@@ -0,0 +1,5 @@
+export const intro = () => {
+    const year = 2025;
+    console.log(`Welcome to a javascript refresher for ${year}`);
+  };
+  
---
+++ main.js
@@ -0,0 +1,8 @@
+import {intro} from './intro.js'
+
+// playground to refresh on javascript in 2024
+const main = () => {
+    intro();
+}
+
+main();
