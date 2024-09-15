88aab05d73e1f0c1f6e9e1cb65880f1b89efb487
Bradley Floyd III <bfloydiii@protonmail.com>
2024-09-15 00:45:16 -0400

    added a class

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 import { welcome } from "./welcome.js";
 import { arrayTest } from "./arrayTest.js";
+import { Car } from "./car.js";
 
 // playground to refresh on javascript in 2024
 const main = () => {
   welcome(); // greeting to the javascript refresher
+  let car = new Car("Ford Mustang", "2024");
 
   let testArr = arrayTest();
   testArr.push("matthew");
   let result = testArr.find((person) => person === "brad");
   console.log(`result is : ${result}`);
   console.log(`testArr is : ${testArr}`);
+  console.log(`Car is: ${car.name}, ${car.year}`);
 };
 
 main();
